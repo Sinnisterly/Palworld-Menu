03f7ded05fcbeb9cf0625448a7e2c239cf2a0e79
Sinnisterly <60723887+Sinnisterly@users.noreply.github.com>
2024-04-15 09:46:29 -0700

    Update config.h
    updated offsets for latest game version

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ class config
 public:
 	//offsets
 	DWORD64 ClientBase = 0;
-	DWORD64 offset_Tick = 0x2B1A220;//APalPlayerCharacter::Tick 48 89 5C 24 ? 57 48 83 EC ? 48 8B F9 E8 ? ? ? ? 48 8B 8F
+	DWORD64 offset_Tick = 0x2B1A910;//APalPlayerCharacter::Tick 48 89 5C 24 ? 57 48 83 EC ? 48 8B F9 E8 ? ? ? ? 48 8B 8F
 	//check
 	bool IsESP = false;
 	bool IsForgeMode = false;
